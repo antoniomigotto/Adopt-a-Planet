d99a60245ab678a4d8941549a572b7699fb97f3d
Antonio Migotto <antoniomigotto@ufl.edu>
2023-01-28 10:22:53 -0500

    Expanded on the foundation, added login page and route to login from the dashboard, added some code to both templates

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Adopt-a-Planet
 A website where unique people adopt unique planets and create unique pages for their unknown worlds.
+
+## Setup
+Run `flask run` in the adopt-a-planet directory and connect to http://localhost:5000 on your local machine.
--- adopt-a-planet/app.py
+++ adopt-a-planet/app.py
@@ -5,6 +5,17 @@ app = Flask(__name__)
 
 @app.route('/')
 def dashboard():
+    """TODO: Docstring for dashboad.
+    :returns: TODO
     """
+    return render_template('dashboard.html')
+
+@app.route('/login')
+def login():
+    """TODO: Docstring for login.
+    :returns: TODO
     """
-    pass
+    return render_template('login.html')
+
+if __name__=="__main__":
+    app.run(debug=True)

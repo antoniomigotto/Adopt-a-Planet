ae466715c1171217a075e470bd9c612468d9a165
Antonio Migotto <antoniomigotto@ufl.edu>
2023-01-29 02:39:03 -0500

    Linting files for readability and consistency

--- adopt-a-planet/app.py
+++ adopt-a-planet/app.py
@@ -73,18 +73,17 @@ def meet_your_planet():
 @app.route("/planet/<int:planet_id>")
 def planet(planet_id):
     pass
-    #No: 2; Name: 11 Umi b; Num Stars: 1; Discovery Year: 2009; Orbital Period Days: 516.219
-    #The exoplanet 11 Umi b was discovered a while back in 2009. It has 1 star in its system, and has a orbital period of 516 days.
+    # No: 2; Name: 11 Umi b; Num Stars: 1; Discovery Year: 2009; Orbital Period Days: 516.219
+    # The exoplanet 11 Umi b was discovered a while back in 2009. It has 1 star in its system, and has a orbital period of 516 days.
 
-    #No: 76; Name: COCONUTS-2 b; Num Stars: 1; Discovery Year: 2021; Orbital Period Days: 402000000
-    #The exoplanet COCONUTS-2 b was discovered recently in 2021. It has only 1 star in its system, and has an incredibly huge rbital period of 402 million days!
+    # No: 76; Name: COCONUTS-2 b; Num Stars: 1; Discovery Year: 2021; Orbital Period Days: 402000000
+    # The exoplanet COCONUTS-2 b was discovered recently in 2021. It has only 1 star in its system, and has an incredibly huge rbital period of 402 million days!
 
-    #No: 187; Name: GJ 180 b; Num Stars: 1; Discovery Year: 2014; Orbital Period Days: 17.133
-    #The exoplanet GJ 180 b was discovered some time ago in 2014. It has 1 star in its system, and has a really small orbital period of 17 days
+    # No: 187; Name: GJ 180 b; Num Stars: 1; Discovery Year: 2014; Orbital Period Days: 17.133
+    # The exoplanet GJ 180 b was discovered some time ago in 2014. It has 1 star in its system, and has a really small orbital period of 17 days
 
-    #No: 516; Name: HD 12648 b; Num Stars: 1; Discovery Year: 2015; Orbital Period Days: 133.6
-    #The exoplanet HD 12648 b was discovered in 2015. It has 1 star, and has an orbital period of 133 days.
-
+    # No: 516; Name: HD 12648 b; Num Stars: 1; Discovery Year: 2015; Orbital Period Days: 133.6
+    # The exoplanet HD 12648 b was discovered in 2015. It has 1 star, and has an orbital period of 133 days.
 
 
 @app.route("/editor")
